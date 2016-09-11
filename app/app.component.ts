c06987d93ffb9f9851d3b70d1318b75e5bf8a847
linda cai <basket6011@hotmail.com>
2016-09-11 13:24:23 -0700

    create seperate angular components for nav-body and nav-title in index.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+                <h1>My First Angular 2 App</h1>
+                <navbar-body></navbar-body>
+                `
 })
 export class AppComponent { }
---
+++ app/component/navbar-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navbar-body',
+    templateUrl: 'app/view/navbar-body.html',
+    styleUrls: ['app/view/bootstrap.min.css']
+})
+
+export class NavbarComponent {
+    title: string = "Navbar Body";
+}
